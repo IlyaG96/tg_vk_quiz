2532880540b6a4a9df42cc962adf1f035bebcfff
IlyaG96 <ilyag_96@icloud.com>
2022-02-18 15:50:57 +1100

    add func to load questions into dictionary

--- load_questions.py
+++ load_questions.py
@@ -1,4 +1,14 @@
 import json
+from pprint import pprint
+with open(file='questions/6iz36.txt', mode='r', encoding='KOI8-R') as file:
+    text = file.read()
+questions = {}
 
-with open(file='questions/1vs1200.txt', mode='r', encoding='KOI8-R') as file:
-    pass
+text2 = text.split('\n\n')
+for item in text2:
+    if 'Вопрос' in item:
+        question = item
+    if 'Ответ' in item:
+        answer = item
+        questions.update({question: answer})
+
