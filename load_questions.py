53b8c73783ea1a29614ca8ea2fcb2e6f26bc1bac
IlyaG96 <ilyag_96@icloud.com>
2022-02-18 21:30:44 +1100

    fix. now save to dict

--- load_questions.py
+++ load_questions.py
@@ -1,14 +1,16 @@
-import json
 from pprint import pprint
-with open(file='questions/6iz36.txt', mode='r', encoding='KOI8-R') as file:
-    text = file.read()
+with open(file='questions/1vs1201.txt', mode='r', encoding='KOI8-R') as file:
+    raw_text = file.read()
+
 questions = {}
+question_number = 1
 
-text2 = text.split('\n\n')
-for item in text2:
-    if 'Вопрос' in item:
-        question = item
-    if 'Ответ' in item:
-        answer = item
-        questions.update({question: answer})
+quiz_text = raw_text.split('\n\n')
+for sentence in quiz_text:
+    if 'Вопрос' in sentence:
+        question = sentence
+    if 'Ответ' in sentence:
+        answer = sentence
 
+        questions.update({question_number: {question: answer}})
+        question_number += 1
