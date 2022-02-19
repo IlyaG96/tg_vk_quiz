b994c47f06970588f8cf09720b44988ef72955cd
IlyaG96 <ilyag_96@icloud.com>
2022-02-19 13:51:09 +1100

    load all txt files from questions

--- load_questions.py
+++ load_questions.py
@@ -1,16 +1,38 @@
-from pprint import pprint
-with open(file='questions/1vs1201.txt', mode='r', encoding='KOI8-R') as file:
-    raw_text = file.read()
+import random
+from os import listdir
 
-questions = {}
-question_number = 1
 
-quiz_text = raw_text.split('\n\n')
-for sentence in quiz_text:
-    if 'Вопрос' in sentence:
-        question = sentence
-    if 'Ответ' in sentence:
-        answer = sentence
+def generate_questions():
 
-        questions.update({question_number: {question: answer}})
-        question_number += 1
+    questions = {}
+    question_start_number = 1
+    question_files = listdir(path='questions')
+
+    for question_file in question_files:
+
+        with open(file=f'questions/{question_file}', mode='r', encoding='KOI8-R') as file:
+            raw_text = file.read()
+
+        quiz_text = raw_text.split('\n\n')
+
+        for sentence in quiz_text:
+            if 'Вопрос' in sentence:
+                question = sentence
+            if 'Ответ' in sentence:
+                answer = sentence
+
+                questions.update({question_start_number: {question: answer}})
+                question_start_number += 1
+
+    return questions
+
+
+def chose_random_question(questions):
+
+    question_num = random.randint(0, len(questions)-1)
+
+    quiz = questions.get(question_num)
+
+    for quest, ans in quiz.items():
+        return quest, ans
+
