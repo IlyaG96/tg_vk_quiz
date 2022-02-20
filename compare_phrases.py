1578ea5fe7cc1f0338067fa68c3aa809459526e0
IlyaG96 <ilyag_96@icloud.com>
2022-02-20 14:34:02 +1100

    remove redunant prints

--- compare_phrases.py
+++ compare_phrases.py
@@ -3,12 +3,11 @@ from fuzzy_match import algorithims
 
 
 def clear_phrase(phrase):
+
     phrase = phrase.lower().strip('')
+    cleared_phrase = phrase.translate(str.maketrans('', '', punctuation))
 
-    for symbol in punctuation:
-        phrase = phrase.replace(symbol, '')
-
-    return phrase
+    return cleared_phrase
 
 
 def compare_phrases(user_answer, quiz_answer):
@@ -17,7 +16,6 @@ def compare_phrases(user_answer, quiz_answer):
     quiz_answer = clear_phrase(quiz_answer)
 
     sin_compare = algorithims.cosine(user_answer, quiz_answer)
-    trigram_compare = algorithims.trigram(user_answer, quiz_answer)
-    compare_percentage = ((sin_compare + trigram_compare)/2)*100
+    trigram = algorithims.trigram(user_answer, quiz_answer)
 
-    return True if compare_percentage >= 50 else False
+    return True if (sin_compare >= 0.5 or trigram >= 0.5) else False
