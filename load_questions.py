6a52a286f8edc4eaffaa8cdf9c1ccaf079a8e270
IlyaG96 <ilyag_96@icloud.com>
2022-02-20 14:42:01 +1100

    path to questions from .env

--- load_questions.py
+++ load_questions.py
@@ -1,15 +1,20 @@
 from os import listdir
+from environs import Env
+from pathlib import Path
 
 
 def generate_questions():
 
+    env = Env()
+    env.read_env()
+    questions_path = env.str('path_to_questions', 'questions')
     questions = {}
     question_start_number = 1
-    question_files = listdir(path='questions')
+    question_files = listdir(path=questions_path)
 
     for question_file in question_files:
-
-        with open(file=f'questions/{question_file}', mode='r', encoding='KOI8-R') as file:
+        file_path = Path(questions_path, question_file)
+        with open(file=file_path, mode='r', encoding='KOI8-R') as file:
             raw_text = file.read()
 
         quiz_text = raw_text.split('\n\n')
@@ -17,9 +22,10 @@ def generate_questions():
         for sentence in quiz_text:
             if 'Вопрос' in sentence:
                 question = sentence
-            if 'Ответ' in sentence:
-                sentence = sentence.split('Ответ')
-                answer = sentence[1]
+            if 'Ответ:' in sentence:
+                sentence = sentence.split('Ответ:')
+                # we use index 1 because answer always stands after 'Ответ:'
+                answer = sentence[1].strip('\n')
 
                 questions.update({question_start_number: {question: answer}})
                 question_start_number += 1
@@ -31,6 +37,6 @@ def chose_question(questions, question_num):
 
     quiz = questions.get(int(question_num))
 
-    for quest, ans in quiz.items():
-        return quest, ans
+    for question, answer in quiz.items():
+        return question, answer
 
